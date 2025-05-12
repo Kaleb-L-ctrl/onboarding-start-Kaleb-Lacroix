cb7fd06ff148d5293446888ddba6cc40a9808f7a
Kaleb Lacroix <kaleblacroix7476@gmail.com>
2025-05-12 18:20:37 -0400

    changes and updates to SPI perifferal

--- src/SPI_perif.v
+++ src/SPI_perif.v
@@ -20,6 +20,8 @@ module SPI_perriferal (
 
 
     reg SCLKRISE;
+    reg prev_sclk;
+    reg nCSrise;
     reg [1:0] SCLK_sync;
     reg [1:0] ncs_sync;
     reg [1:0] copi_sync;
@@ -27,27 +29,39 @@ module SPI_perriferal (
     reg [4:0] counter;
     reg [7:0] Madd; //message destination adress
     reg[7:0] Mdata; //message data
-    reg [15,0] copi_message;
-
+    reg [15:0] copi_message;
+    
     always @(posedge clk) begin//on internal clock we sample through our buffers
-        SCLK_buffer <= SCLK //avoids metastability with initital FF since we'll use SCLK_sync[0] for SCLKRISE so just one more layer here
+        SCLK_buffer <= SCLK; //avoids metastability with initital FF since we'll use SCLK_sync[0] for SCLKRISE so just one more layer here
         SCLK_sync <= {SCLK_sync[0], SCLK_buffer};
-        copi_sync <= {cpoi_sync[0], copi};
+        prev_sclk <= SCLK_sync[1];
+        copi_sync <= {copi_sync[0], copi};
         ncs_sync <= {ncs_sync[0], nCS};
-        SCLKRISE <= (SCLK_sync[1] == 0)&&(SCLK_sync[0] == 1);
-    end
+        SCLKRISE <= (prev_sclk == 0)&&(SCLK_sync[0] == 1);
+        //nCSfall <= (ncs_sync[0] == 1) &&(ncs_sync[1] == 0);
+        nCSrise <= (ncs_sync[0] == 0) &&(ncs_sync[1] == 1);
+    
 
 
-    always @(SCLKRISE) begin//data valid take a sample and do the thing
-        copi_message <= {copi_message[14:0], cpoi_sync[1]}//load in the new bit.
-        counter += 1;
+    
+        if(SCLKRISE) begin//data valid take a sample and do the thing
+            if (!ncs_sync[1]) begin
+                copi_message <= {copi_message[14:0], copi_sync[1]};//load in the new bit.
+                counter += 1;
 
-        if (counter == 15) begin
-            counter <= 0;
-            if (copi_message[15] == 1'b1) begin///we ignore read
-                Madd<= copi_message[14:7];
-                Mdata <= copi_message[7:0];
-                case (Madd)// ***NTS this takes proroty over PWN enable
+                if (counter == 15) begin
+                    counter <= 0;
+                    if (copi_message[15] == 1'b1) begin///we ignore read
+                        Madd<= copi_message[14:7];
+                        Mdata <= copi_message[7:0];
+                        
+                    end
+                end
+            end
+           
+        end
+         if (nCSrise)begin
+                case (Madd)//log all of the data to the registers when nCS is rising edge
                     7'h00:en_reg_out_7_0  <= Mdata;
                     7'h01:en_reg_out_15_8 <= Mdata;
                     7'h02:en_reg_pwm_7_0  <= Mdata;
@@ -56,13 +70,11 @@ module SPI_perriferal (
                     default:;//do nothing we are ignoring invalid adresses
                 endcase
             end
-        end
     end
+   
 
 
-
-    SCLKRISE = (SCLK_sync ==1)&&( prevSCLK == 0)
-    prevSCLK <= SCLK_sync
+    
   // All output pins must be assigned. If not used, assign to 0.
 
   // List all unused inputs to prevent warnings
