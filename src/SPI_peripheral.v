4dcd7a15ee3321b5efb5599886e9f859b59b4244
Kaleb Lacroix <kaleblacroix7476@gmail.com>
2025-05-14 19:55:41 -0400

    test

--- src/SPI_peripheral.v
+++ src/SPI_peripheral.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_uwasic_onboarding_matthew_chen(
+module tt_um_uwasic_onboarding_kaleb_lacroix(
 
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
