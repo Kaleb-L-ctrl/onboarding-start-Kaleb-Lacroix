d3eb40c1bf32e12d5c0c9fb6b93ec025e3eca9eb
Kaleb Lacroix <kaleblacroix7476@gmail.com>
2025-05-14 15:48:21 -0400

    further debugging and changes to project V

--- src/project.v
+++ src/project.v
@@ -51,7 +51,7 @@ module tt_um_uwasic_onboarding_kaleb_lacroix (
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
     .pwm_duty_cycle(pwm_duty_cycle),
-    .out({uio_out, uo_out})
+    .out(out)
   );
 
 
