0397dd37f0fad9831773de67fb769a380a9fed59
Kaleb Lacroix <kaleblacroix7476@gmail.com>
2025-05-14 19:54:34 -0400

    test

--- src/SPI_peripheral.v
+++ src/SPI_peripheral.v
@@ -1,110 +1,73 @@
 /*
- * Copyright (c) 2025 Kaleb Lacroix
+ * Copyright (c) 2024 Your Name
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module SPI_peripheral (
-    input wire SCLK,
-    input wire nCS,
-    input wire COPI,
-    input wire clk,
-    input wire rst_n,
+module tt_um_uwasic_onboarding_matthew_chen(
 
-    output reg [7:0] en_reg_out_7_0,
-    output reg [7:0] en_reg_out_15_8,
-    output reg [7:0] en_reg_pwm_7_0,
-    output reg [7:0] en_reg_pwm_15_8,
-    output reg [7:0] pwm_duty_cycle
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n    // reset_n - low to reset
+    //input  wire [1:0] test_mode // debugger. unsupported by make. moved to ui_in[4:3]
 );
 
+// always@(*) begin
+//   case(addr_out)
+//     7'd0: uo_out <= en_reg_out_7_0;
+//     7'd1: uo_out <= en_reg_out_15_8;
+//     7'd2: uo_out <= en_reg_pwm_7_0;
+//     7'd3: uo_out <= en_reg_pwm_15_8;
+//     7'd4: uo_out <= pwm_duty_cycle;
+//     default: uo_out <= 8'b0;
+//   endcase
+// end
 
+  wire [15:0] out;
+  
+  assign uo_out  = out[7:0];
+  assign uio_out = out[15:8];
+  assign uio_oe  = 0;
 
-    wire SCLKRISE;
-    wire nCSrise;
-    reg prev_sclk;
-    
-    reg [1:0] SCLK_sync;
-    reg [1:0] ncs_sync;
-    reg [1:0] copi_sync;
-   
-   reg message_ready;
-    reg [4:0] counter;
-    reg [6:0] Madd; //message destination adress
-    reg[7:0] Mdata; //message data 
-    reg [15:0] copi_message;
-    
+    // Create wires to connect to the values of the registers
+  wire [7:0] en_reg_out_7_0;
+  wire [7:0] en_reg_out_15_8;
+  wire [7:0] en_reg_pwm_7_0;
+  wire [7:0] en_reg_pwm_15_8;
+  wire [7:0] pwm_duty_cycle;
 
-    assign SCLKRISE = (SCLK_sync == 2'b01);
-    assign nCSrise =  (ncs_sync == 2'b01);
+  pwm_peripheral pwm_peripheral_inst (
+      .clk(clk),
+      .rst_n(rst_n),
+      .en_reg_out_7_0(en_reg_out_7_0),
+      .en_reg_out_15_8(en_reg_out_15_8),
+      .en_reg_pwm_7_0(en_reg_pwm_7_0),
+      .en_reg_pwm_15_8(en_reg_pwm_15_8),
+      .pwm_duty_cycle(pwm_duty_cycle),
+      //.out()
+      .out(out) //[15:8] to uio, [7:0] to uo
+    );
+    // Add uio_in and ui_in[7:5] to the list of unused signals:
+    wire _unused = &{ena, ui_in[7:3], uio_oe, uio_in, 1'b0};
 
-    always @(posedge clk or negedge rst_n) begin//on internal clock we sample through our buffers
-        if (!rst_n)begin //reset (active low)
-            SCLK_sync       <= 2'b00;
-            copi_sync       <= 2'b00;
-            ncs_sync        <= 2'b00;
-            prev_sclk       <= 0;
+   spi_peripheral spi_peripheral_inst (
+      .SCLK(ui_in[0]),
+      .COPI(ui_in[1]),
+      .nCS(ui_in[2]),
+      .clk(clk),
+      .rst_n(rst_n),
 
-            en_reg_out_7_0  <= 8'b0;
-            en_reg_out_15_8 <= 8'b0;
-            en_reg_pwm_7_0  <= 8'b0;
-            en_reg_pwm_15_8 <= 8'b0;
-            pwm_duty_cycle  <= 8'b0;
-
-            counter         <= 0;
-            copi_message    <= 16'b0;
-            Madd            <= 8'b0;
-            Mdata           <= 8'b0;
-            message_ready   <=0;
-
-        end else begin//not reset; we capture values from contrtoler
-            SCLK_sync <= {SCLK_sync[0], SCLK};
-            prev_sclk <= SCLK_sync[1];
-            copi_sync <= {copi_sync[0], COPI};
-            ncs_sync  <= {ncs_sync[0], nCS};
-        
-    
-        
-        
-        if(SCLKRISE) begin//data valid take a sample and run code, (SCKRISE will always be 0 on rst)
-            if (!ncs_sync == 2'b00) begin
-                copi_message <= {copi_message[14:0], copi_sync[1]};//load in the new bit.
-                counter <= counter + 1;
-
-                if (counter == 16) begin
-                    counter <= 0;
-                    message_ready <= 1'b1;
-                end
-            end
-           
-        end
-        if (message_ready) begin///we ignore read
-            message_ready <=0;
-            if (copi_message[15]== 1'b1)begin
-                Madd  <= copi_message[14:8];
-                Mdata <= copi_message[7:0];
-                
-                case (Madd)//log all of the data to the registers when nCS is rising edge
-                    7'h00:en_reg_out_7_0  <= Mdata;
-                    7'h01:en_reg_out_15_8 <= Mdata;
-                    7'h02:en_reg_pwm_7_0  <= Mdata;
-                    7'h03:en_reg_pwm_15_8 <= Mdata;
-                    7'h04:pwm_duty_cycle  <= Mdata;
-                    default: ;//do nothing we are ignoring invalid adresses
-                endcase
-            end
-        end
-     
-    end
-end
-   
-
-
-    
-  // All output pins must be assigned. If not used, assign to 0.
-
-  // List all unused inputs to prevent warnings
-  //wire _unused = &{ena, clk, rst_n, 1'b0};
+      .en_reg_out_7_0(en_reg_out_7_0),
+      .en_reg_out_15_8(en_reg_out_15_8),
+      .en_reg_pwm_7_0(en_reg_pwm_7_0),
+      .en_reg_pwm_15_8(en_reg_pwm_15_8),
+      .pwm_duty_cycle(pwm_duty_cycle)
+    );
 
 endmodule
