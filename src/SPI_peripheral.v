f0e79afbae9789011318f29d0fa96a7b323a8ec5
Kaleb Lacroix <kaleblacroix7476@gmail.com>
2025-05-14 20:03:25 -0400

    test

--- src/SPI_peripheral.v
+++ src/SPI_peripheral.v
@@ -1,71 +1,110 @@
-module SPI_peripheral #(
-    parameter SYNC_FLOPS = 2
-)
-(
-    input wire       SCLK,// clock
-    input wire       COPI,//in from controller
-    input wire       nCS,//start transaction on negedge
-    input wire       clk,//
-    input wire       rst_n,// 
+/*
+ * Copyright (c) 2025 Kaleb Lacroix
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module SPI_peripheral (
+    input wire SCLK,
+    input wire nCS,
+    input wire COPI,
+    input wire clk,
+    input wire rst_n,
 
     output reg [7:0] en_reg_out_7_0,
-    output reg [7:0] en_reg_out_15_8, 
+    output reg [7:0] en_reg_out_15_8,
     output reg [7:0] en_reg_pwm_7_0,
     output reg [7:0] en_reg_pwm_15_8,
     output reg [7:0] pwm_duty_cycle
 );
 
-reg [4:0] curr_bit; 
-reg [15:0] transaction_data;
 
-reg [SYNC_FLOPS-1:0] SCLK_sync;
-reg [SYNC_FLOPS-1:0] COPI_sync;
-reg [SYNC_FLOPS-1:0] nCS_sync;
 
-always @(posedge clk or negedge rst_n) begin
+    wire SCLKRISE;
+    wire nCSrise;
+    reg prev_sclk;
+    
+    reg [1:0] SCLK_sync;
+    reg [1:0] ncs_sync;
+    reg [1:0] copi_sync;
+   
+   reg message_ready;
+    reg [4:0] counter;
+    reg [6:0] Madd; //message destination adress
+    reg[7:0] Mdata; //message data 
+    reg [15:0] copi_message;
+    
 
-    if (!rst_n) begin //reset state
-        curr_bit <= 5'b0;
-        transaction_data <= 16'b0;
+    assign SCLKRISE = (SCLK_sync == 2'b01);
+    assign nCSrise =  (ncs_sync == 2'b01);
 
-        SCLK_sync <= 0;
-        COPI_sync <= 0;
-        nCS_sync <= 0;
+    always @(posedge clk or negedge rst_n) begin//on internal clock we sample through our buffers
+        if (!rst_n)begin //reset (active low)
+            SCLK_sync       <= 2'b00;
+            copi_sync       <= 2'b00;
+            ncs_sync        <= 2'b00;
+            prev_sclk       <= 0;
 
-        en_reg_out_7_0 <= 8'b0;
-        en_reg_out_15_8 <= 8'b0;
-        en_reg_pwm_7_0 <= 8'b0;
-        en_reg_pwm_15_8 <= 8'b0;
-        pwm_duty_cycle <= 8'b0;
-    end else begin
-        //capture on clock cycle
-        SCLK_sync <= {SCLK_sync[SYNC_FLOPS-2:0], SCLK};
-        COPI_sync <= {COPI_sync[SYNC_FLOPS-2:0], COPI};  // Use COPI_sync
-        nCS_sync <= {nCS_sync[SYNC_FLOPS-2:0], nCS};     // Use nCS_sync
+            en_reg_out_7_0  <= 8'b0;
+            en_reg_out_15_8 <= 8'b0;
+            en_reg_pwm_7_0  <= 8'b0;
+            en_reg_pwm_15_8 <= 8'b0;
+            pwm_duty_cycle  <= 8'b0;
+
+            counter         <= 0;
+            copi_message    <= 16'b0;
+            Madd            <= 8'b0;
+            Mdata           <= 8'b0;
+            message_ready   <=0;
+
+        end else begin//not reset; we capture values from contrtoler
+            SCLK_sync <= {SCLK_sync[0], SCLK};
+            prev_sclk <= SCLK_sync[1];
+            copi_sync <= {copi_sync[0], COPI};
+            ncs_sync  <= {ncs_sync[0], nCS};
         
-        if(nCS_sync == 2'b10) begin //negedge. begin data capture
-            curr_bit <= 5'b0;
-            transaction_data <= 16'b0;
-        end
+    
+        
+        
+        if(SCLKRISE) begin//data valid take a sample and run code, (SCKRISE will always be 0 on rst)
+            if (!ncs_sync == 2'b00) begin
+                copi_message <= {copi_message[14:0], copi_sync[1]};//load in the new bit.
+                counter <= counter + 1;
 
-        else if (nCS_sync == 2'b00 && SCLK_sync == 2'b01) begin //posedge detect SCLK when data is valid
-            if (curr_bit!=5'b10000) begin
-                    transaction_data[15 - curr_bit] <= COPI_sync[SYNC_FLOPS-1];
-                    curr_bit <= curr_bit + 1;
+                if (counter == 16) begin
+                    counter <= 0;
+                    message_ready <= 1'b1;
                 end
+            end
+           
         end
-        if(curr_bit[4] == 1'b1 && transaction_data[15] == 1'b1) begin
-            case (transaction_data[14:8]) 
-                7'b0000000: en_reg_out_7_0 <= transaction_data[7:0];
-                7'b0000001: en_reg_out_15_8 <= transaction_data[7:0];
-                7'b0000010: en_reg_pwm_7_0 <= transaction_data[7:0];
-                7'b0000011: en_reg_pwm_15_8 <= transaction_data[7:0];
-                7'b0000100: pwm_duty_cycle <= transaction_data[7:0];
-                default: ;
-            endcase
+        if (message_ready==1'b1) begin///we ignore read
+            message_ready <=0;
+            if (copi_message[15]== 1'b1)begin
+                Madd  <= copi_message[14:8];
+                Mdata <= copi_message[7:0];
+                
+                case (Madd)//log all of the data to the registers when nCS is rising edge
+                    7'h00:en_reg_out_7_0  <= Mdata;
+                    7'h01:en_reg_out_15_8 <= Mdata;
+                    7'h02:en_reg_pwm_7_0  <= Mdata;
+                    7'h03:en_reg_pwm_15_8 <= Mdata;
+                    7'h04:pwm_duty_cycle  <= Mdata;
+                    default: ;//do nothing we are ignoring invalid adresses
+                endcase
+            end
         end
+     
     end
 end
+   
 
+
+    
+  // All output pins must be assigned. If not used, assign to 0.
+
+  // List all unused inputs to prevent warnings
+  //wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
